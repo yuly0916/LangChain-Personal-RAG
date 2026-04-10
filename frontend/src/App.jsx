9b4914debe5f5aa14dce37fd21cbb5d88832f4bc
yuly0916 <yuly09162@gmail.com>
2026-04-11 02:25:46 +0900

    feat(frontend): root page path modify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,12 @@ import Login from './page/Login'
 function App() {
 
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path='/login' element={<Login/>}/>
-    </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
