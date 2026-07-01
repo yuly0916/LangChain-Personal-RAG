8532dec0b5290c6bcc55288c88717f479286a5fb
yuly0916 <yuly09162@gmail.com>
2026-07-02 03:46:22 +0900

    로그인 페이지 생성

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -1,11 +1,57 @@
+import "./Login.css";
 
 const Login = () => {
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&response_type=code&prompt=login`;
+  const handleKakaoLogin = () => {
+    const params = new URLSearchParams({
+      client_id: import.meta.env.VITE_CLIENT_ID,
+      redirect_uri: import.meta.env.VITE_REDIRECT_URI,
+      response_type: "code",
+      prompt: "login",
+    });
 
-    return (
-        <>
-        </>
-    )
-}
+        window.location.href = `https://kauth.kakao.com/oauth/authorize?${params.toString()}`;
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-card">
+        <div className="login-profile">
+          <div className="login-profile-icon">
+            <img src="/favicon.ico" alt="서비스 아이콘" />
+          </div>
+
+          <div>
+            <h1>로그인 해주세요!</h1>
+            <p>
+              토끼에게 물어보려면
+              <br />
+              카카오 로그인이 필요해요.
+            </p>
+          </div>
+        </div>
+
+        <div className="login-message-box">
+          <p>
+            로그인 후 서비스를
+            <br />
+            바로 이용할 수 있어요.
+          </p>
+        </div>
+
+        <button
+          type="button"
+         className="kakao-login-button"
+          onClick={handleKakaoLogin}
+        >
+          <img src="/kakao_login.png" alt="카카오 로그인" />
+        </button>
+
+        <p className="login-sub-text">
+          카카오 계정으로 간편하게 시작하세요.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
